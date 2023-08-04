84c54f32930db04445eed792aed2c1cdd0476a70
furkanpolat06 <emnplt.0698@gmail.com>
2023-08-04 09:53:25 +0300

    data edit

--- src/helper/data.js
+++ src/helper/data.js
@@ -8,10 +8,6 @@ const data = [
     description: "A high-performance executive car from BMW's M series.",
     category: "sedan",
     image: "https://cdn.motor1.com/images/mgl/OpP3e/s3/2021-bmw-m5-m5-competition.jpg",
-    rating: {
-      rate: 4.9,
-      count: 100,
-    },
   },
   
   {
@@ -23,10 +19,6 @@ const data = [
     description: "A legendary sports car known for its speed and performance.",
     category: "coupe",
     image: "https://www.auto-data.net/images/f69/Nissan-GT-R-R35-facelift-2023.jpg",
-    rating: {
-      rate: 4.8,
-      count: 90,
-    },
   },
   {
     id: 4,
@@ -37,10 +29,6 @@ const data = [
     description: "A sporty muscle car with great performance and style.",
     category: "coupe",
     image: "https://www.motortrend.com/uploads/2022/10/17-2023-Chevrolet-Camaro-RS-Exterior.jpg",
-    rating: {
-      rate: 4.6,
-      count: 60,
-    },
   },
   {
     id: 5,
@@ -51,10 +39,6 @@ const data = [
     description: "A luxurious SUV with advanced features and comfort.",
     category: "suv",
     image: "https://media.ed.edmunds-media.com/bmw/x5/2021/oem/2021_bmw_x5_4dr-suv_xdrive45e_fq_oem_1_1600.jpg",
-    rating: {
-      rate: 4.9,
-      count: 200,
-    },
   },
   {
     id: 7,
@@ -65,10 +49,6 @@ const data = [
     description: "An off-road capable SUV with a rugged design.",
     category: "suv",
     image: "https://static.motor.es/fotos-noticias/2023/04/jeep-wrangler-2024-202394130-1680714947_1.jpg",
-    rating: {
-      rate: 4.7,
-      count: 180,
-    },
   },
   {
     id: 25,
@@ -79,10 +59,6 @@ const data = [
     description: "A luxurious and powerful coupe from BMW's M series.",
     category: "coupe",
     image: "https://www.arabazzi.com/images/yuklemeler/bmw-m31323.jpg",
-    rating: {
-      rate: 4.7,
-      count: 80,
-    },
   },
   {
     id: 8,
@@ -93,10 +69,6 @@ const data = [
     description: "An electric sedan with cutting-edge technology.",
     category: "sedan",
     image: "https://www.cumhuriyet.com.tr/Archive/2022/4/8/1923876/kapak_161111.jpg",
-    rating: {
-      rate: 4.9,
-      count: 300,
-    },
   },
   {
     id: 22,
@@ -107,10 +79,6 @@ const data = [
     description: "A high-performance sports car from Audi's RS series.",
     category: "sports",
     image: "https://media.ed.edmunds-media.com/audi/rs-7/2021/oem/2021_audi_rs-7_sedan_base_fq_oem_2_1600.jpg",
-    rating: {
-      rate: 4.7,
-      count: 120,
-    },
   },
   {
     id: 9,
@@ -121,10 +89,6 @@ const data = [
     description: "A premium compact SUV with a comfortable and stylish interior.",
     category: "suv",
     image: "https://cdn.ototeknikveri.com/Files/News/Big/2021-audi-q5-ve-sq5-sportback-avrupaya-geldi.jpg",
-    rating: {
-      rate: 4.5,
-      count: 120,
-    },
   },
   {
     id: 10,
@@ -135,10 +99,6 @@ const data = [
     description: "A versatile wagon with all-wheel drive for outdoor adventures.",
     category: "wagon",
     image: "https://www.autozeitung.de/assets/styles/article_image/public/field/images/subaru-outback-2021-01.jpg?itok=dXbDVlEV",
-    rating: {
-      rate: 4.4,
-      count: 90,
-    },
   },
   {
     id: 11,
@@ -149,10 +109,6 @@ const data = [
     description: "A luxury compact car with a prestigious brand image.",
     category: "luxury",
     image: "https://s3-prod.autonews.com/s3fs-public/AMGCV43-MAIN_i.jpg",
-    rating: {
-      rate: 4.8,
-      count: 250,
-    },
   },
   {
     id: 12,
@@ -163,10 +119,6 @@ const data = [
     description: "A reliable and capable full-size pickup truck.",
     category: "pickup",
     image: "https://file.kelleybluebookimages.com/kbb/base/evox/StJ/13673/2021-GMC-Sierra%201500%20Double%20Cab-front-angle3_13673_089_640x480.jpg",
-    rating: {
-      rate: 4.6,
-      count: 170,
-    },
   },
   {
     id: 13,
@@ -177,10 +129,6 @@ const data = [
     description: "A safe and elegant SUV with advanced safety features.",
     category: "suv",
     image: "https://www.autoscout24.at/cms-content-assets/227905GMBoSi62u3laFcN4-70d4b9e78c36b4ec9f48d01854e00895-Volvo_XC60__2_-1100.jpg",
-    rating: {
-      rate: 4.7,
-      count: 110,
-    },
   },
   {
     id: 14,
@@ -192,10 +140,6 @@ const data = [
     description: "A premium crossover SUV with a smooth and comfortable ride.",
     category: "suv",
     image: "https://assets.flowfound.com/cars/2023/2023%20Lexus%20RX50.jpg",
-    rating: {
-      rate: 4.5,
-      count: 190,
-    },
   },
   {
     id: 15,
@@ -206,10 +150,6 @@ const data = [
     description: "An iconic sports car with breathtaking performance.",
     category: "coupe",
     image: "https://cdn.hiconsumption.com/wp-content/uploads/2022/08/2023-Porsche-911-GT3-RS-0-Hero.jpg",
-    rating: {
-      rate: 4.9,
-      count: 80,
-    },
   },
   {
     id: 3,
@@ -220,10 +160,6 @@ const data = [
     description: "A powerful pickup truck for heavy-duty work and towing.",
     category: "pickup",
     image: "https://api.ford-koegler.de/fileadmin/Modellreihe/F150/Lightning/Text-Bild-Ford_F-150_Lariat__Willkommen_in_der_Koenigsklasse-Desktop-2x.jpg",
-    rating: {
-      rate: 4.8,
-      count: 120,
-    },
   },
   {
     id: 19,
@@ -234,10 +170,6 @@ const data = [
     description: "A compact SUV with modern design and advanced features.",
     category: "suv",
     image: "https://www.arabahabercisi.com/wp-content/uploads/2021/04/2021-Hyundai-Tucson-fiyat%C4%B1-600x381.jpg",
-    rating: {
-      rate: 4.3,
-      count: 120,
-    },
   },
   {
     id: 6,
@@ -248,10 +180,6 @@ const data = [
     description: "A family-friendly midsize sedan with a spacious interior.",
     category: "sedan",
     image: "https://cdn.jdpower.com/JDP_2023%20Nissan%20Altima%20SL%20Garnet%20Pearl%20Red%20Front%20Quarter%20View.jpg",
-    rating: {
-      rate: 4.3,
-      count: 150,
-    },
   },
   
   
@@ -264,10 +192,6 @@ const data = [
     description: "A reliable and comfortable sedan for everyday use.",
     category: "sedan",
     image: "https://content.toyota.com.ph/uploads/prices/701/002_701_1639107449853_000.jpg",
-    rating: {
-      rate: 4.5,
-      count: 100,
-    },
   },
   
   {
@@ -279,10 +203,6 @@ const data = [
     description: "A popular compact car known for its fuel efficiency.",
     category: "sedan",
     image: "https://www.honda.com.tr/assets/img/civic/home-banner.png",
-    rating: {
-      rate: 4.2,
-      count: 85,
-    },
   },
   {
     id: 16,
@@ -293,10 +213,6 @@ const data = [
     description: "A compact SUV with good fuel efficiency and practicality.",
     category: "suv",
     image: "https://www.autoscout24.de/cms-content-assets/2ZjAqbTWPlKduPthSUeAgv-878ba15231976e106d8a58d7f0a7faee-chevrolet-equinox-front-1100.jpeg",
-    rating: {
-      rate: 4.3,
-      count: 140,
-    },
   },
   
 ]
